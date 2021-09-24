47f0d123708a26110531295476e10f2752b2a943
Honepa <cot1884@gmail.com>
2021-09-24 12:34:56 +0500

    dshki

---
+++ get_coor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 13:06:40 2021
+
+@author: honepa
+"""
+
+from dshki import d 
+"""
+rq = requests.get('https://nominatim.openstreetmap.org/search?format=json&q=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&polygon_geojson=1')
+
+coor = rq.json()
+
+json_ob = json.dumps(coor)
+
+with open("json/first_coor.json", "w") as outfile:
+    outfile.write(json_ob)
+
+print(json_ob)
+"""
+print(d)
+
